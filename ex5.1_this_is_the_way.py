4f51a3d6af5fe985733be5a68902029e06e4bcba
Rachel Dekel <dekelrachel@gmail.com>
2022-03-24 14:41:32 +0200

    fix

--- ex5.1_this_is_the_way.py
+++ ex5.1_this_is_the_way.py
@@ -1,12 +1,16 @@
 import os
-INPUT_FOLDER_MESSAGE="insert the path to the folder:"
-# function that print the file in folder that start with "deep"
-def print_deep_folder():
-    folder_path = input(INPUT_FOLDER_MESSAGE)
-    directory_list = os.listdir(folder_path)
-    only_deep = [f for f in directory_list if f.startswith("deep")]
-    print(only_deep)
 
-#print the program
+
+def print_deep_folder():
+    """
+    this function returns all the files that start with the word deep
+    :return: the deep files in the path
+    """
+    folder_path = input("Insert the path to the folder:")
+    directory_list = os.listdir(folder_path)
+    deep_files = [f for f in directory_list if f.startswith("deep")]
+    print(deep_files)
+
+
 if __name__ == '__main__':
     print_deep_folder()
