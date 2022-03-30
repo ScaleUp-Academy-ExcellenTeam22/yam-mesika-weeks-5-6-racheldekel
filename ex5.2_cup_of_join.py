4b18cd861ad2ddbc316afd6075f544a3fdffe6a4
Rachel Dekel <dekelrachel@gmail.com>
2022-03-30 11:09:57 +0300

    fixing ex5.2

--- ex5.2_cup_of_join.py
+++ ex5.2_cup_of_join.py
@@ -1,9 +1,16 @@
-#funcation that add the seperator for each list
+
 def print_join(*lists, seperator='-'):
-    list_with= [cell for curr_list in lists for cell in curr_list + [seperator]][:-1]
-    if list_with ==[]:
-        return 'empty'
+    """
+    uncation that add the seperator for each list
+    :param lists:
+    :param seperator:
+    :return:
+    """
+    list_with = [place for curr_list in lists for place in curr_list + [seperator]][:-1]
+    if not list_with:
+        return ()
     return list_with
 
+
 if __name__ == '__main__':
-    print(print_join([1,2],[8],[9,5,6],seperator='@'))
+    print(print_join([1, 2], [8], [9, 5, 6], seperator='@'))
